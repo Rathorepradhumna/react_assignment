67a1e31c8813281d13f9247c5558dbb3a1097756
Rathorepradhumna <pradhumnarathore1998@gmail.com>
2020-09-04 23:26:14 +0530

    hello world using class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+ render() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
     </div>
   );
+ }
 }
 
 export default App;
